5e2b9b5745f95fcdf47bc1cc5c3488c5a37dc8e8
IgnacioOrellana <ignacio97orellana@gmail.com>
2022-11-11 21:31:41 -0300

    port added

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { DB_PORT } = process.env;
+const PORT = process.env.PORT || 3002
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(DB_PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
